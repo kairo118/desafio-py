a92a278946b69111ab0cf7f8236b76a486bb6be6
GuilhermePain <skairo118@gmail.com>
2024-05-04 23:21:27 -0300

    primeiro commit

---
+++ desafio py.py
@@ -0,0 +1,18 @@
+import seaborn as sns
+import pandas as pd
+
+data = {
+'year': [1949, 1949, 1949, 1949, 1949],
+'month': ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+'passengers': [112, 118, 132, 129, 121]
+}
+
+flights = pd.DataFrame(data)
+
+monthly_passengers = flights.groupby('month').passengers.sum()
+
+max_month = monthly_passengers.idxmax()
+max_passengers = monthly_passengers.max()
+
+print(f'O mês com o maior número de passageiros é {max_month} com um total de {max_passengers} passageiros.')
+
